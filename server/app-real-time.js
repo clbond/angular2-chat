cd8bc8c388545311f1be706f4f5412907ce9253f
Chris Bond <chris.bond@rangle.io>
2016-07-19 21:10:53 -0400

    Route messages back to sender in case you want to see your own presence or send messages to self

--- server/app-real-time.js
+++ server/app-real-time.js
@@ -3,9 +3,11 @@
 module.exports = (app, io) => {
   const onConnect = (socket) => {
     socket.on('message', (payload) => {
+      socket.emit('message', payload);
       socket.broadcast.emit('message', payload);
     });
     socket.on('presence', (payload) => {
+      socket.emit('presence', payload);
       socket.broadcast.emit('presence', payload);
     })
   };
